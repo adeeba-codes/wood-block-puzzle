b7e2bae4c94b3c4cdc3596f81d42a1a147297cda
adeeba-codes <adeebafaraz8@gmail.com git config --global user.name adeeba-codesssh-keygen -t ed25519 -C adeebafaraz8@gmail.com>
2025-11-30 19:48:16 +0530

    Connected frontend to backend

--- frontend/api.js
+++ frontend/api.js
@@ -1,5 +1,5 @@
 // api.js
-const API_BASE = "http://localhost:5000/api";
+const API_BASE = "https://woodblock-backend.onrender.com";
 
 // Load token & user from localStorage
 let authToken = localStorage.getItem("token") || null;
